ac6fc3cbdc0bfcc35cd6034b51957a8380103fed
Vedant Mishra <vedantmishra893@gmail.com>
2025-01-24 10:42:51 +0530

    [Updated: Parent Folder changes]

--- real_state/src/Subcomponents/About.jsx
+++ real_state/src/Subcomponents/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <section id='aboutUs_Mini'>
         <div className='first_container'>
             <div className='content'>
-                <h1>ABOUT US</h1>
+                <h1>ABOUT Us</h1>
                 <p>Discover the epitome of luxury living in our exquisite villas, meticulously designed to offer unparalleled comfort and elegance. Nestled in serene locations, our villas provide a perfect blend of modern amenities and natural beauty, ensuring a tranquil and sophisticated lifestyle.</p>
                 <p>Enjoy beautifully landscaped gardens, private swimming pools, and outdoor entertainment areas perfect for hosting gatherings or enjoying quiet moments of relaxation.</p>
                 <p>Our villas are strategically located in the most sought-after neighborhoods, offering convenient access to essential amenities such as schools, hospitals, shopping centers, and recreational facilities. Whether you desire a peaceful retreat or a vibrant community, we have the perfect villa for you.</p>
